4fc9f29e5caaf29b17c83cb809f2aacfa7e207c1
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-11-30 11:04:28 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { initializeFirebase } from './firebase.config'
+import * as express from 'express'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	app.use(express.json({ limit: '50mb' }))
+	app.use(express.urlencoded({ limit: '50mb', extended: false }))
+	app.enableCors()
+	app.useGlobalPipes(new ValidationPipe())
+	await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
+initializeFirebase()
