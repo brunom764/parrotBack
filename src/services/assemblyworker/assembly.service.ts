e86d65e99eafeefe20b5462226cd8ce6817d3b53
Ana Júlia <100807129+AnaJulia22@users.noreply.github.com>
2023-12-12 15:15:11 +0000

    organização do código

--- src/services/assemblyworker/assembly.service.ts
+++ src/services/assemblyworker/assembly.service.ts
@@ -1,12 +1,10 @@
 import { Injectable} from '@nestjs/common'
 import fs from 'fs-extra'
 import axios from 'axios'
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 @Injectable()
 export class AssemblyService{ 
-        api_token = process.env.ASSEMBLY_API_TOKEN
+        api_token: string = process.env.ASSEMBLY_API_TOKEN || '';
 
         // Function to upload a local file to the AssemblyAI API
         async upload_file(api_token: string, path: string): Promise<string | null> {
@@ -15,16 +13,16 @@ export class AssemblyService{
 
             try {
                 const response = await axios.post(url, data, {
-                headers: {
-                    'Content-Type': 'application/octet-stream',
-                    'Authorization': api_token,
-                },
+                    headers: {
+                        'Content-Type': 'application/octet-stream',
+                        'Authorization': api_token,
+                    },
                 });
 
                 if (response.status === 200) {
-                return response.data['upload_url'];
+                    return response.data['upload_url'];
                 } else {
-                console.error(`Error: ${response.status} - ${response.statusText}`);
+                    console.error(`Error: ${response.status} - ${response.statusText}`);
                 return null;
                 }
             } catch (error) {
@@ -35,9 +33,9 @@ export class AssemblyService{
 
         // Async function that sends a request to the AssemblyAI transcription API and retrieves the transcript
         async transcribeAudio(api_token: string, audio_url: string) {
-        const headers = {
-            authorization: api_token,
-            'content-type': 'application/json',
+            const headers = {
+                authorization: api_token,
+                'content-type': 'application/json',
         };
 
         // Send a POST request to the transcription API with the audio URL in the request body
@@ -59,15 +57,15 @@ export class AssemblyService{
 
             // If the transcription is complete, return the transcript object
             if (transcriptionResult.status === 'completed') {
-            return transcriptionResult;
+                return transcriptionResult;
             }
             // If the transcription has failed, throw an error with the error message
             else if (transcriptionResult.status === 'error') {
-            throw new Error(`Transcription failed: ${transcriptionResult.error}`);
+                throw new Error(`Transcription failed: ${transcriptionResult.error}`);
             }
             // If the transcription is still in progress, wait for a few seconds before polling again
             else {
-            await new Promise((resolve) => setTimeout(resolve, 3000));
+                await new Promise((resolve) => setTimeout(resolve, 3000));
             }
         }
     }
