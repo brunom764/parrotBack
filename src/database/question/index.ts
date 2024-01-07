6b0f861d6b45b4eb67116f41f66ecd8760faafdb
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-07 16:48:12 -0300

    feat(question-database): adicionar métodos para criar perguntas e resumos

---
+++ src/database/question/index.ts
@@ -0,0 +1,62 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/services/prisma.service'
+
+@Injectable()
+export class QuestionDataBase {
+	constructor(@Inject(PrismaService) protected prisma: PrismaService) {}
+
+	async createQuestion(id: string, transId: string, question: string) {
+		await this.prisma.question.create({
+			data: {
+				id,
+				transId,
+				question
+			}
+		})
+	}
+
+	async createSummary(id: string, summary: string) {
+		await this.prisma.transcription.update({
+			where: {
+				id
+			},
+			data: {
+				summary
+			}
+		})
+	}
+
+	// async createAnswer(id: string, transId: string, answer: string) {
+	// 	await this.prisma.question.update({
+	// 		where: {
+	// 			id,
+	// 			transId
+	// 		},
+	// 		data: {
+	// 			answer
+	// 		}
+	// 	})
+	// }
+
+	async getQuestionById(id: string) {
+		await this.prisma.question.findUnique({
+			where: {
+				id
+			}
+		})
+	}
+
+	async getQuestionsByTransId(transId: string) {
+		await this.prisma.question.findMany({
+			where: {
+				transId
+			},
+			select: {
+				id: true,
+				question: true,
+				answer: true,
+				createdAt: true
+			}
+		})
+	}
+}
