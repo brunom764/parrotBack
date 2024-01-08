138e8ab7f9da288fda58b05517a3c6e48a9fa1ff
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2024-01-08 01:11:55 -0300

    feat(question): adiciona question.module

--- src/core/question/question.module.ts
+++ src/core/question/question.module.ts
@@ -1,4 +1,12 @@
-import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/services/prisma.service'
+import { Module } from '@nestjs/common'
+import { QuestionService } from './question.service'
+import { QuestionDataBase } from 'src/database/question'
+import { QuestionController } from './question.controller'
 
-@Module({})
+@Module({
+	controllers: [QuestionController],
+	providers: [QuestionService, PrismaService, QuestionDataBase],
+	exports: [QuestionService]
+})
 export class QuestionModule {}
