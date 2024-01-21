f9588db6a074507a4925547fa511bd5d99e6eecd
Gabriel Alves <gabrielalvesacardoso@gmail.com>
2024-01-20 21:54:07 -0300

    Adicionando as funcionalidades do chat gpt para resumo e resposta

--- src/services/openai/openai.service.ts
+++ src/services/openai/openai.service.ts
@@ -1,16 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { TranscriptText } from '@core/transcription/entities'
 import { Injectable } from '@nestjs/common'
+import { OpenAI } from "openai";
+
+const openai = new OpenAI();
 
 @Injectable()
 export class OpenaiService {
 	api_token: string = process.env.OPENAI_API_TOKEN
 
 	async genereateAnswer(question: string, context: TranscriptText[]) {
-		return 'answer'
+		const answer = await openai.chat.completion.create({
+			messages: [{role: "system", content: `Responda ${question} com base no texto: ${context}`}],
+			model: "gpt-3.5-turbo"
+		})
+		return answer.choices[0].message.context
 	}
 
 	async generateSummary(context: TranscriptText[]) {
-		return 'sumarry'
+		const sumarry = await openai.chat.completion.create({
+			messages: [{role: "system", content: `Por favor resuma este texto, tomando cuidado
+			para não perder o contexto: ${context}`}],
+			model: "gpt-3.5-turbo"
+		})
+		return sumarry.choices[0].message.context
 	}
 }
